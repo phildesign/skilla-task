9f824ed6ace1625bc93ed5819a16826d43d1daca
Phil <phil.nice.23@gmail.com>
2024-09-23 20:57:43 +0300

    feat: add time formatting

--- src/components/Duration/Duration.tsx
+++ src/components/Duration/Duration.tsx
@@ -3,7 +3,9 @@ import { DurationProps } from './Duration.props';
 import styles from './Duration.module.scss';
 
 const Duration = ({ time }: DurationProps): JSX.Element => {
-	return <div className={styles.root}>{time}</div>;
+	const formattedTime = `${Math.floor(time / 60)}:${time % 60}`;
+
+	return <div className={styles.root}>{formattedTime}</div>;
 };
 
 export default Duration;
