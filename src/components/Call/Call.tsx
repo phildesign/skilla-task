14735084d85357b33d3db856790cf1f4e480241c
Phil <phil.nice.23@gmail.com>
2024-09-23 20:34:50 +0300

    feat: add formatting for phone number

--- src/components/Call/Call.tsx
+++ src/components/Call/Call.tsx
@@ -1,9 +1,13 @@
+import { parsePhoneNumber, PhoneNumber } from 'libphonenumber-js';
 import { CallProps } from './Call.props';
 
 import styles from './Call.module.scss';
 
 const Call = ({ phone }: CallProps): JSX.Element => {
-	return <div className={styles.root}>{phone}</div>;
+	const parsedNumber: PhoneNumber = parsePhoneNumber(phone, 'RU');
+	const formattedNumber = `+7${parsedNumber.formatNational().slice(1)}`;
+
+	return <div className={styles.root}>{formattedNumber}</div>;
 };
 
 export default Call;
